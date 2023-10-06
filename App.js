b3342c0947a4b00dae5152c7f54c113d51ab1aeb
Marja Nykänen <marja.nykanen@gofore.com>
2023-10-06 13:54:08 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const speak = () => {
+    const thingToSay = text;
+    Speech.speak(thingToSay);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Type to hear' onChangeText={text => setText(text)} value={text} />
+      <Button title="Press to hear some words" onPress={speak} />
     </View>
   );
 }
